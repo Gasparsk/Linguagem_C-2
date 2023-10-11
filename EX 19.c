680dfdd629cfbf71228211d30059efa1f35303ec
katask <143566536+katask@users.noreply.github.com>
2023-10-11 17:00:14 -0300

    Add files via upload

---
+++ EX 19.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+
+    int i;
+    float soma, nota, media;
+
+    for (i = 1; i <= 3; i++)
+    {
+        do
+        {
+            printf("Digite sua %d nota: ", i);
+            scanf("%f", &nota);
+        } while (nota < 0 || nota > 10);
+
+        soma += nota;
+        media = soma / i;
+    }
+
+   
+
+    if (media >= 7)
+    {
+        printf("Media: %.2f \n", media);
+        printf("Aprovado!");
+    }
+    else if (media >= 5 || media < 7)
+    {
+        printf("Media: %.2f \n", media);
+        printf("Recuperacao!");
+    }
+    else if (media < 5)
+    {
+        printf("Media: %.2f \n", media);
+        printf("Reprovado!");
+    }
+
+    return 0;
+}
